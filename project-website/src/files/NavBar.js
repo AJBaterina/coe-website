9a6416a8f34f607b8f1a61017574bca8f9c963be
AJBaterina <104185594+AJBaterina@users.noreply.github.com>
2024-11-10 20:34:29 +0800

    initailization only

---
+++ project-website/src/files/NavBar.js
@@ -0,0 +1,27 @@
+import "../styles/NavBar.css";
+
+function NavBar() {
+	return (
+		<div id="nav-body">
+			asdfasfdasdf alan
+			<div id="nav-logo"></div>
+			<div id="nav-items">
+				<a></a>
+			</div>
+			<div id="nav-items">
+				<a></a>
+			</div>
+			<div id="nav-items">
+				<a></a>
+			</div>
+			<div id="nav-items">
+				<a></a>
+			</div>
+			<div id="nav-items">
+				<a></a>
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
